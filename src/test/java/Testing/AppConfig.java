a9d15d1c1853caf20d417eb2de16394a991fde62
SantoshMurthy007 <santoshmurthy04@gmail.com>
2024-04-19 21:20:19 +0530

    first commit

---
+++ src/test/java/Testing/AppConfig.java
@@ -0,0 +1,28 @@
+package Testing;
+
+public class AppConfig {
+    // User registration data
+	
+	public static final String URL = "https://orikan-ui-automation-test.azurewebsites.net/";
+    public static final String USERNAME = "san@orikan.com";
+    public static final String PASSWORD = "Test@123";
+    public static final String FIRST_NAME = "Santosh";
+    public static final String LAST_NAME = "Murthy";
+
+    // Personal information data
+    public static final String ADDRESS_LINE1 = "RocksSjow Lane";
+    public static final String ADDRESS_LINE2 = "3467 Pl";
+    public static final String POSTCODE = "500056";
+    public static final String CITY = "SpartansBurg";
+    public static final String STATE = "South Carolina";
+
+    // Payment information data
+    public static final String CARD_HOLDER_NAME = "Santosh";
+    public static final String CARD_NUMBER = "43794392039449320";
+    public static final String CARD_CVV = "456";
+    public static final String CARD_EXPIRY_MONTH = "June";
+    public static final String CARD_EXPIRY_YEAR = "2030";
+    
+    public static final String USERNAME2 = "carl@orikan.com";
+    public static final String PASSWORD2 = "Test@123";
+}
