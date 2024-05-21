a5314fd646bccb96a6fa62cbfb50ba09e7b06b4a
namal <namal@DESKTOP-8UO4728>
2024-05-21 09:43:21 +0530

    My first commit

--- src/test/java/Testing/OrikanLocators.java
+++ src/test/java/Testing/OrikanLocators.java
@@ -20,15 +20,6 @@ public class OrikanLocators {
 	    public static final By STATE_SELECT = By.id("state");
 	    public static final By REGISTER_BUTTON_STEP2 = By.xpath("/html/body/app-root/div/div[2]/app-register-wizard/div[3]/button[2]");
 
-	    // Payment Information Page Locators
-	    public static final By CARD_HOLDER_NAME_INPUT = By.id("cardHolderName");
-	    public static final By CARD_TYPE_VISA_RADIO = By.id("cardTypeVISA");
-	    public static final By CARD_NUMBER_INPUT = By.id("cardNumber");
-	    public static final By CARD_CVV_INPUT = By.id("cardCVV");
-	    public static final By CARD_EXPIRY_MONTH_SELECT = By.id("cardExpiryMonth");
-	    public static final By CARD_EXPIRY_YEAR_INPUT = By.id("cardExpiryYear");
-	    public static final By REGISTER_BUTTON_STEP3 = By.xpath("/html/body/app-root/div/div[2]/app-register-wizard/div[3]/button[2]");
-
 	    // Terms and Conditions Page Locators
 	    public static final By AGREE_TO_TERMS_CHECKBOX = By.id("agreedToTerms");
 	    public static final By REGISTER_BUTTON_STEP4 = By.xpath("/html/body/app-root/div/div[2]/app-register-wizard/div[3]/button[2]");
